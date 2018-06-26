6a654f0d4ae48014ba5413c1761c408c90350dc6
mars <ma407g@gmail.com>
2018-06-26 17:24:41 +0100

    Commit to the Remote in GitHub

---
+++ SongsApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SongsApp
+//
+//  Created by Marc Jackson on 02/12/2017.
+//  Copyright © 2017 Marc Jackson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
